99365d34631199a05279bd9118ff0f1e6eeb0350
Whiskey <13729557916@163.com>
2025-05-06 17:09:35 +0800

    Update sigma_shiny.R

--- R/sigma_shiny.R
+++ R/sigma_shiny.R
@@ -54,54 +54,55 @@ renderSgraph <- function(expr, env = parent.frame(), quoted = FALSE) {
 sigma_from_igraph <- function(igraph, layout = NULL, label_color = '#fff',
                               width = "100%", height = "400px",
                               elementId = NULL, label_grid_cell_size = 200) {
-
+  
   directed_flag <- igraph::is_directed(igraph)
   graph_parse <- igraph::as_data_frame(igraph, what = 'both')
-
+  
   edges <- graph_parse$edges[, c('from', 'to')]
   edges[c('from', 'to')] %<>% lapply(as.character)
   colnames(edges) <- c('source', 'target')
   edges$key <- seq_len(nrow(edges))
   edges$size <- 1
   edges$color <- '#636363'
-
+  
   nodes <- graph_parse$vertices
   nodes$label <- row.names(nodes)
   layout = if (is.null(layout)) igraph::layout_nicely(igraph) else layout
   nodes <- cbind(nodes[, 'label', drop = FALSE], layout)
   colnames(nodes) <- c('label', 'x', 'y')
-
+  
   nodes$key <- seq_len(nrow(nodes))
   nodes$size <- 3
   nodes[c('x', 'y')] %<>% lapply(as.numeric)
   nodes$color <- '#3182bd'
-
+  
   edges$source %<>% match(nodes$label) %>% `[`(nodes$key, .)
   edges$target %<>% match(nodes$label) %>% `[`(nodes$key, .)
-
+  
   nodes$label %<>% as.character
   edges[c('source', 'target')] %<>% lapply(as.character)
-
+  
   # adapt changes for v2.4.0
   nodes %<>% graph_to_json 
   edges %<>% graph_to_json('edges')
-
+  
   graph_out <- list(nodes = nodes, edges = edges, directed = directed_flag)
-
+  
   sigma_opts <- list(min_node_size = 1, max_node_size = 3, min_edge_size = 1,
-                  max_edge_size = 3, neighborEvent = 'onClick',
-                  neighborStart = 'clickNode', neighborEnd = 'clickStage',
-                  doubleClickZoom = TRUE, mouseWheelZoom = TRUE,
-                  edge_arrows = 'def', label_color = label_color,
-                  label_grid_cell_size = label_grid_cell_size)
-
+                     max_edge_size = 3, neighborEvent = 'onClick',
+                     neighborStart = 'clickNode', neighborEnd = 'clickStage',
+                     doubleClickZoom = TRUE, mouseWheelZoom = TRUE,
+                     edge_arrows = 'def', label_color = label_color,
+                     label_grid_cell_size = label_grid_cell_size)
+  
   graph_json <- jsonlite::toJSON(graph_out, auto_unbox = TRUE)
   sigma_obj <- list(data = graph_json, options = sigma_opts,
                     graph = graph_parse)
-
+  
   createWidget(name = 'sgraph', sigma_obj, width = width, height = height,
                package = 'sgraph', elementId = elementId)
 }
+
 
 # separate df in keys fields and attributes
 graph_to_json = function(df_input, type = c('nodes', 'edges')) {
